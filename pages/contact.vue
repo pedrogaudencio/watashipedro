94dd8e3c8388d743c4a7afd1b1f47a2410431a23
Pedro Gaudêncio <pmgaudencio@gmail.com>
2019-08-11 18:26:27 +0900

    contact: adds contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,23 +1,35 @@
 <template>
-  <div>
-    <navigation />
-    <h1>hey.</h1>
+  <div class="content">
+    <article class="center">
+      <section>
+      <input type="hidden" name="contact" netlify-honeypot="bot-field" value="contact" />
+      <form name="contact" action="/thank-you" method="post" netlify>
+        <label class="form-label" for="name">
+          Name:
+        </label>
+        <input class="form-field" name="name" id="name" />
+        <label class="form-label" for="email">
+          Email:
+        </label>
+        <input class="form-field" name="email" id="email" />
+        <label class="form-label" for="message">
+          Message:
+        </label>
+        <textarea class="form-field" name="message" id="message"></textarea>
+        <input class="form-button" type="submit" value="Send message" />
+        <p class="hidden"><label>Don’t fill this out: <input name="bot-field"></label></p>
+      </form>
+      </section>
+    </article>
   </div>
 </template>
 
 <script>
-  import Navigation from '../components/navigation';
 
-  export default {
-    components: {
-      Navigation
-    }
-  }
 </script>
 
 <style>
-  h1 {
-    margin-left: auto;
-    margin-right: auto;
-  }
+.hidden {
+  display: none;
+}
 </style>
